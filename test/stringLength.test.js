fb6a7cc66c9830a4f968284e05eaf55c46ae588a
Shinn Thant Swam Ye <1shinnthantswamye@gmail.com>
2023-08-26 12:25:50 +0630

    Add test for stringLength function

---
+++ test/stringLength.test.js
@@ -0,0 +1,21 @@
+const stringLength = require('../tasks/stringLength');
+
+test('Return correct length of string', () => {
+    const inputString = "Hello";
+    const result = stringLength(inputString);
+    expect(result).toBe(5);
+});
+
+test('Throws error for empty string', () => {
+    const inputString = '';
+    expect(() => {
+        stringLength(inputString);
+    }).toThrow('String length must be at least one character long!')
+});
+
+test('Throws error for string longer than 10 characters', () => {
+    const inputString = 'More than 10 characters';
+    expect(() => {
+        stringLength(inputString);
+    }).toThrow('String length must not be longer than 10 characters!')
+});
