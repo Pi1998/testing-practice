31ca7e6610028fd60a1532bf25cf061631db0369
Shinn Thant Swam Ye <1shinnthantswamye@gmail.com>
2023-08-26 12:21:41 +0630

    Add test for reverString function

---
+++ test/reverseString.test.js
@@ -0,0 +1,13 @@
+import reverseString from "../tasks/reverseString";
+
+test('Check whether function returns reverse string', () => {
+    const inputString = "Hello";
+    const result = reverseString(inputString);
+    expect(result).toBe("olleH");
+});
+
+test('Compare the indexes before and after reverseString', () => {
+    const inputString = "Hamburger";
+    const result = reverseString(inputString);
+    expect(result[0]).toBe(inputString[inputString.length-1]);
+});
