102794414a9a0601d10951f2c3d28ef68e71e933
Shinn Thant Swam Ye <1shinnthantswamye@gmail.com>
2023-08-26 12:25:22 +0630

    Add stringLength function

---
+++ tasks/stringLength.js
@@ -0,0 +1,13 @@
+  function stringLength(string) {
+    if (string.length >= 1 && string.length <= 10) {
+        return string.length;
+    } else {
+        if (string.length === 0) {
+            throw new Error('String length must be at least one character long!');
+        } else {
+            throw new Error('String length must not be longer than 10 characters!')
+        }
+    }
+};
+
+module.exports = stringLength;
